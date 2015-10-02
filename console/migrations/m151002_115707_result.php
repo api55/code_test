18f89cdf255b4cc6dbf09e2d7de98990aaa5290c
Pablo Aponte <pablo.aponte@gmail.com>
2015-10-02 13:58:38 +0200

    Database Migration

---
+++ console/migrations/m151002_115707_result.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151002_115707_result extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m151002_115707_result cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
